d5f3d06128b6f307d8d6a1e5d6b3e3a851ca7d90
Vlad Nedelcu <nedelcuvd@gmail.com>
2026-02-14 22:04:45 +0200

    Cleanup - Removed & renamed discovery

--- apps/web/src/features/Discovery/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DiscoveryPage } from "./DiscoveryPage";
-export { useAnonymousAuth } from "./hooks/useAnonymousAuth";
-export { useConvertAnonymous } from "./hooks/useConvertAnonymous";
