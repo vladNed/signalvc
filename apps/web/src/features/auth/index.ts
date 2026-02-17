f600bb58535783b8ccd0fa79a04229a439676ea5
Vlad Nedelcu <nedelcuvd@gmail.com>
2026-02-17 12:22:42 +0200

    Fix - Fix verify page

--- apps/web/src/features/auth/index.ts
+++ apps/web/src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthPage } from "./AuthPage";
 export { getUserEmail, getUserName } from "./utils/profileHelpers";
 export { upsertProfile } from "./utils/upsertProfile";
+export { VerifyPage } from "./VerifyPage";
